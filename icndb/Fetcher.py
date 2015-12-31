5a05591a3097c1d286331848b6468cad201b3319
Timur Mullayanov <TimurM@bitbucket.org>
2016-01-01 03:07:02 +0500

    code cleanup. Replaced outdated #TODO with a new one

--- icndb/Fetcher.py
+++ icndb/Fetcher.py
@@ -3,11 +3,11 @@ __author__ = "Timur Mullayanov"
 import urllib.request
 import json
 
-#TODO: implement class derived from any Exception for negative joke number
-#class
-
 __baseURL__ = 'http://api.icndb.com/'
 
+#TODO: add feature with names replacement
+#NOTE: final name just should be passed as argument to all FETCH functions
+# and correctply be passed to web-DB
 
 def fetchRandom(number=1):
     checkNumber(number) # raise an Exception if number is invalid
@@ -17,6 +17,7 @@ def fetchRandom(number=1):
         return _requestJokes("{}/{}".format(url, number))
     else:
         return _requestJokes(url)
+
 
 def _requestJokes(url):
     return urllib.request.urlopen(url).read().decode('utf-8')
