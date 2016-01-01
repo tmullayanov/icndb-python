10b1a92fd1cfc8fa593778e9cabf4f62db37c1a1
Timur Mullayanov <TimurM@bitbucket.org>
2016-01-02 01:07:14 +0500

    fixed typos which prevented default __str__ and __repr__ from being overriden; fixed logic error which broke object instantiation

--- icndb/Joke.py
+++ icndb/Joke.py
@@ -6,9 +6,11 @@ class Joke(object):
     Class represents joke from ICNDB.
     All the fields are READ-ONLY.
     '''
+    __slots__ = ("_id", "_text")
+
     def __init__(self, id, text):
-        self._id = id
-        self._text = text
+        super(Joke, self).__setattr__("_id", id)
+        super(Joke, self).__setattr__("_text", text)
 
     def __setattr__(self, key, value):
         raise AttributeError("Set is not allowed!")
@@ -21,8 +23,8 @@ class Joke(object):
     def text(self):
         return self._text
 
-    def str(self):
-        return "[Joke #{0}]{1}".format(self._id, self._text)
+    def __str__(self):
+        return "\"[Joke #{0}] {1}\"".format(self._id, self._text)
 
-    def repr(self):
+    def __repr__(self):
         return self.__str__()
