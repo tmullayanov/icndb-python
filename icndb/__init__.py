113cf6be8259727e3c33675c630c7c2f6128598f
Timur Mullayanov <TimurM@bitbucket.org>
2016-01-03 22:43:46 +0500

    made newly implemented methods visible at module level

--- icndb/__init__.py
+++ icndb/__init__.py
@@ -1,7 +1,7 @@
 __author__ = 'Timur Mullayanov'
 
 # move all API methods and entities to package-level scope.
-from icndb.Fetcher import fetchRandom
+from icndb.Fetcher import fetchRandom, fetchByID, fetchCategories
 from icndb.Joke import Joke as Joke_
 
 del Fetcher
