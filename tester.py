ffbee03777fe945264a22351f5c1b085d306d577
Timur Mullayanov <TimurM@bitbucket.org>
2016-01-03 23:06:18 +0500

    more use-cases

--- tester.py
+++ tester.py
@@ -5,8 +5,15 @@ def main():
     print("Categories: {}".format(icndb.fetchCategories()))
     print("Getting random joke from icndb")
     print(icndb.fetchRandom())
-    print("Getting joke about Octocat GitHub")
+    print("\nGetting joke about Octocat GitHub")
     print(icndb.fetchRandom(number=1, firstName="Octocat", lastName="GitHub"))
+    print("\nGetting joke by ID: 13\n{}".format(icndb.fetchByID(13)))
+
+    print("\nGetting 3 jokes in one turn:")
+    print(icndb.fetchRandom(number=3))
+
+    print("\nGetting random joke from NERDY category")
+    print(icndb.fetchRandom(limitTo=["nerdy"]))
 
 
 if __name__ == '__main__':
