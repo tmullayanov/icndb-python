9d994392a26aefcc8de1701008bf6bbe000276e9
Timur Mullayanov <TimurM@bitbucket.org>
2016-01-03 22:44:21 +0500

    added more "test cases" to use-case script

--- tester.py
+++ tester.py
@@ -2,8 +2,12 @@
 import icndb
 
 def main():
+    print("Categories: {}".format(icndb.fetchCategories()))
     print("Getting random joke from icndb")
     print(icndb.fetchRandom())
+    print("Getting joke about Octocat GitHub")
+    print(icndb.fetchRandom(number=1, firstName="Octocat", lastName="GitHub"))
+
 
 if __name__ == '__main__':
     main()
